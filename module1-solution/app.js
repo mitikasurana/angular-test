e6829c842e6eb78ac1dc23abdf4cd7105970ac12
mitikasurana <62178061+mitikasurana@users.noreply.github.com>
2020-09-09 20:46:14 +0530

    Final commit

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -12,12 +12,33 @@
             console.log(items.length);
             if(list==""){
                 $scope.result = "Please enter data first";
+                $scope.fontCol = {
+                    "color" : "red",
+                    "font-weight" : "bold"
+                };
+                $scope.boxStyle = {
+                    "border-color" : "red"
+                };
             }
             else if(list!="" && items.length <= 3){
                 $scope.result = "Enjoy!"
+                $scope.fontCol = {
+                    "color" : "green",
+                    "font-weight" : "bold"
+                };
+                $scope.boxStyle = {
+                    "border-color" : "green"
+                };
             }
             else if(items.length > 3){
                 $scope.result = "Too much!"
+                $scope.fontCol = {
+                    "color" : "red",
+                    "font-weight" : "bold"
+                };
+                $scope.boxStyle = {
+                    "border-color" : "red"
+                };
             }
         };
     };
